7cc02c1ef6a131d201d1a61b43a971365586555a
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-01-22 21:08:01 +0530

    Create Create,Write and read a file.cpp

---
+++ File handling/Create,Write and read a file.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main() {
+  // Create and open a text file
+  ofstream MyFile("filename.txt");
+
+  // Write to the file
+  MyFile << "Files can be tricky, but it is fun enough!";
+
+  // Close the file
+  MyFile.close();
+  string myText;
+
+// Read from the text file
+ifstream MyReadFile("filename.txt");
+
+// Use a while loop together with the getline() function to read the file line by line
+while (getline (MyReadFile, myText)) {
+  // Output the text from the file
+  cout<<"Reading:\t";
+  cout << myText;
+}
+
+// Close the file
+MyReadFile.close();
+}
