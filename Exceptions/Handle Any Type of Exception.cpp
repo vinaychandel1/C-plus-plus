85653012bcaa4e6f1cf62f10ee11ba6f3c8797bc
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-01-22 21:37:16 +0530

    Create Handle Any Type of Exception.cpp

---
+++ Exceptions/Handle Any Type of Exception.cpp
@@ -0,0 +1,21 @@
+/*
+Handle Any Type of Exceptions (...)
+If you do not know the throw type used in the try block, you can use the "three dots" syntax (...) inside the catch block, which will handle any type of exception:*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  try {
+    int age = 15;
+    if (age >= 18) {
+      cout << "Access granted - you are old enough.";
+    } else {
+      throw 505;
+    }
+  }
+  catch (int myNum) {
+    cout << "Access denied - You must be at least 18 years old.\n";
+    cout << "Error number: " << myNum;  
+  }
+  return 0;
+}
