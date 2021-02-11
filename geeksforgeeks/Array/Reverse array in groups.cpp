c5b6d80e8f61eaba4297b3e41bd1f008f1608b46
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-02-11 23:08:14 +0530

    Create Reverse array in groups.cpp

---
+++ geeksforgeeks/Array/Reverse array in groups.cpp
@@ -0,0 +1,17 @@
+class Solution{
+public:
+    void reverseInGroups(vector<long long>& arr, int n, int k){
+      for (int i = 0; i < n; i += k) 
+    { 
+        int left = i; 
+  
+        // to handle case when k is not multiple of n 
+        int right = min(i + k - 1, n - 1); 
+  
+        // reverse the sub-array [left, right] 
+        while (left < right) 
+            swap(arr[left++], arr[right--]); 
+  
+    } 
+    }
+};
