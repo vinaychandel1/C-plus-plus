8629b7f942ef635dfc9f34833fa641fe8790f3a8
VIINNAY CHANDEL <38590400+vinaychandel1@users.noreply.github.com>
2021-02-23 13:42:26 +0530

    Create smallest prime factor.cpp

---
+++ Sieve of Eratosthenes/smallest prime factor.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+
+void primefactor(int n){
+int spf[100]={0};
+for (int i = 2; i <=n; ++i)
+{
+			spf[i]=i;
+		
+}
+for (int i = 2; i <=n; ++i)
+{
+	if (spf[i]==i)
+	{
+		for(int j=i*i;j<=n;j+=i){
+			if (spf[j]==j)
+			{
+				spf[j]=i;
+			}
+		}
+	}
+}
+while(n!=1){
+	cout<<spf[n]<<" ";
+	n=n/spf[n];
+}
+
+}
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin>>n;
+	primefactor(n);
+
+	return 0;
+}
